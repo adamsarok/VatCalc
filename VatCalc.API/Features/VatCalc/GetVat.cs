465abf2f0c34945d8027755cd6127d83e105fe1e
Ádám Sarok <sarok.adam@gmail.com>
2025-04-23 21:13:21 +0200

    feat: negative tests

--- VatCalc.API/Features/VatCalc/GetVat.cs
+++ VatCalc.API/Features/VatCalc/GetVat.cs
@@ -1,16 +1,38 @@
 ﻿
+using MediatR;
+
 namespace VatCalc.API.Features.VatCalc;
 public static class GetVat {
 	public record VatQuery(decimal? NetEur, decimal? GrossEur, decimal? VatAmountEur, decimal VatRate)
 		: IRequest<VatResponse>;
-	public record VatResponse(decimal? NetEur, decimal? GrossEur, decimal? VatAmountEur, decimal VatRate);
-	public class VatHandler : IRequestHandler<VatQuery, VatResponse> {
-		public Task<VatResponse> Handle(VatQuery request, CancellationToken cancellationToken) {
+	public record VatResponse {
+		static readonly decimal[] validVatRates = { 10, 13, 20 };
+		public decimal? NetEur { get; }
+		public decimal? GrossEur { get; } 
+		public decimal? VatAmountEur { get; } 
+		public decimal VatRate { get; }
+		public VatResponse(decimal? netEur, decimal? grossEur, decimal? vatAmountEur, decimal vatRate) {
+			if (!validVatRates.Contains(vatRate)) throw new InvalidVatRateException();
+			if (netEur == 0 || grossEur == 0 || vatAmountEur == 0) throw new InvalidAmountException();
+			var validInputCnt = new[] { netEur, grossEur, vatAmountEur }.Count(x => x != null);
+			if (validInputCnt > 1) throw new MoreThanOneInputException();
+			if (validInputCnt < 1) throw new InvalidAmountException();
+			NetEur = netEur;
+			GrossEur = grossEur;
+			VatAmountEur = vatAmountEur;
+			VatRate = vatRate;
+			//TODO
 			throw new NotImplementedException();
 		}
 	}
+
+	public class VatHandler : IRequestHandler<VatQuery, VatResponse> {
+		public async Task<VatResponse> Handle(VatQuery request, CancellationToken cancellationToken) {
+			return new VatResponse(request.NetEur, request.GrossEur, request.VatAmountEur, request.VatRate);
+		}
+	}
 	public class InvalidAmountException : Exception {
-		public InvalidAmountException(string fieldName) : base($"Missing or invalid amount input") { }
+		public InvalidAmountException() : base($"Missing or invalid amount input") { }
 	}
 	public class MoreThanOneInputException : Exception {
 		public MoreThanOneInputException() : base("More than one input amount provided. Please set only one of gross, net, VAT amounts.") { }
---
+++ Vatcalc.Tests/UnitTests/VatCalc/VatCalc.cs
@@ -0,0 +1,60 @@
+﻿using static VatCalc.API.Features.VatCalc.GetVat;
+
+namespace Vatcalc.Tests.UnitTests.VatCalc;
+public class VatCalc {
+
+	[Theory]
+	[InlineData(100, 10, 110, 10)]
+	[InlineData(13, 1.69, 14.69, 13)]
+	[InlineData(150000, 30000, 180000, 20)]
+	public async Task Calc(decimal netValueEur, decimal grossValueEur, decimal vatAmountEur, decimal vatRate) {
+		var handler = new VatHandler();
+		var netInputResponse = await handler.Handle(new VatQuery(netValueEur, null, null, vatRate), new CancellationToken());
+		ValidateResponse(netValueEur, grossValueEur, vatAmountEur, netInputResponse);
+		var grossInputResponse = await handler.Handle(new VatQuery(null, grossValueEur, null, vatRate), new CancellationToken());
+		ValidateResponse(netValueEur, grossValueEur, vatAmountEur, grossInputResponse);
+		var vatAmountInputResponse = await handler.Handle(new VatQuery(null, null, vatAmountEur, vatRate), new CancellationToken());
+		ValidateResponse(netValueEur, grossValueEur, vatAmountEur, vatAmountInputResponse);
+	}
+	private static void ValidateResponse(decimal netValueEur, decimal grossValueEur, decimal vatAmountEur, VatResponse response) {
+		Assert.Equal(netValueEur, response.NetEur);
+		Assert.Equal(grossValueEur, response.GrossEur);
+		Assert.Equal(vatAmountEur, response.VatAmountEur);
+	}
+	[Fact]
+	public async Task MissingOrInvalidAmountInput() {
+		var handler = new VatHandler();
+		await Assert.ThrowsAsync<InvalidAmountException>(async () => {
+			await handler.Handle(new VatQuery(null, null, null, 20), new CancellationToken());
+		});
+	}
+	[Fact]
+	public async Task MoreThanOneInput() {
+		var inputs = new List<VatQuery>() {
+			new VatQuery(100m, 10m, null, 10m),
+			new VatQuery(13m, null, 14.69m, 13m),
+			new VatQuery(null, 30000m, 180000m, 20m)
+		};
+		var handler = new VatHandler();
+		foreach (var input in inputs) {
+			await Assert.ThrowsAsync<MoreThanOneInputException>(async () => {
+				await handler.Handle(input, new CancellationToken());
+			});
+		}
+	}
+	[Fact]
+	public async Task InvalidVatRateInput() {
+		var inputs = new List<VatQuery>() {
+			new VatQuery(100m, 10m, null, 10.1m),
+			new VatQuery(13m, null, 14.69m, 10000m),
+			new VatQuery(null, 30000m, 180000m, -5m),
+			new VatQuery(null, 30000m, 180000m, 0m),
+		};
+		var handler = new VatHandler();
+		foreach (var input in inputs) {
+			await Assert.ThrowsAsync<InvalidVatRateException>(async () => {
+				await handler.Handle(input, new CancellationToken());
+			});
+		}
+	}
+}
