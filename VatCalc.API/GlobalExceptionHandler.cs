53d803cc753a88742bb8d52715babf0065372fca
Ádám Sarok <sarok.adam@gmail.com>
2025-04-25 22:34:12 +0200

    feat: exception handler, health check, openAPI

---
+++ VatCalc.API/GlobalExceptionHandler.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using static VatCalc.API.Features.VatCalc.GetVat;
+
+namespace VatCalc.API;
+public sealed class GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger) : IExceptionHandler {
+	public async ValueTask<bool> TryHandleAsync(
+		HttpContext httpContext,
+		Exception exception,
+		CancellationToken cancellationToken) {
+		logger.LogError(
+			exception, "Exception occurred: {Message} at {Time}", exception.Message, DateTime.UtcNow);
+
+		(string Detail, string Title, int StatusCode) details = exception switch {
+			InvalidAmountException or MoreThanOneInputException or InvalidVatRateException => (
+				exception.Message,
+				exception.GetType().Name,
+				StatusCodes.Status400BadRequest
+			),
+			_ => (
+				exception.Message,
+				exception.GetType().Name,
+				StatusCodes.Status500InternalServerError
+			)
+		};
+		httpContext.Response.StatusCode = details.StatusCode;
+		var problemDetails = new ProblemDetails {
+			Title = details.Title,
+			Detail = details.Detail,
+			Status = details.StatusCode,
+			Instance = httpContext.Request.Path
+		};
+		problemDetails.Extensions.Add("traceId", httpContext.TraceIdentifier);
+		await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+		return true;
+	}
+}
--- VatCalc.API/Program.cs
+++ VatCalc.API/Program.cs
@@ -1,14 +1,36 @@
-var builder = WebApplication.CreateBuilder(args);
+using HealthChecks.UI.Client;
+using VatCalc.API;
 
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddOpenApi();
 builder.Services.AddCarter();
 var assembly = typeof(Program).Assembly;
 builder.Services.AddMediatR(config => {
 	config.RegisterServicesFromAssembly(assembly);
 });
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
+builder.Services.AddCors(options => {
+	options.AddPolicy("AllowSpecificOrigin",
+		builder => builder.AllowAnyOrigin()
+						  .AllowAnyHeader()
+						  .AllowAnyMethod());
+});
+builder.Services.AddHealthChecks();
 
 var app = builder.Build();
-
+if (app.Environment.IsDevelopment()) {
+	app.MapOpenApi();
+}
 app.MapCarter();
+app.UseExceptionHandler();
+app.UseCors(x => x.AllowAnyHeader()
+	.AllowAnyMethod()
+	.AllowCredentials()
+	.WithOrigins("http://localhost"));
+app.UseHealthChecks("/health", new Microsoft.AspNetCore.Diagnostics.HealthChecks.HealthCheckOptions {
+	ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
 
 app.Run();
 
